6808e18325e39131256842fc8b5210cc51a68a4a
Artem Vainshtein <64923345+Artemissssss@users.noreply.github.com>
2023-07-16 22:00:31 +0200

    Update authorization.js

--- src/pages/api/authorization.js
+++ src/pages/api/authorization.js
@@ -3,6 +3,8 @@ const { MongoClient, ObjectId } = require('mongodb');
 
 export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', 'https://localhost:3000 https://goiteens-platform-doc.vercel.app');
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     console.log(req.body)
     console.log(req.method)
     if(req.method === "POST"){
