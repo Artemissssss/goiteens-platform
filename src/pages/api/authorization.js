76d6c4ef7e43a0995559453914722bc53f5c25df
Artem Vainshtein <64923345+Artemissssss@users.noreply.github.com>
2023-07-16 20:51:05 +0200

    Update authorization.js

--- src/pages/api/authorization.js
+++ src/pages/api/authorization.js
@@ -3,10 +3,9 @@ const { MongoClient, ObjectId } = require('mongodb');
 
 export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
-res.setHeader('Access-Control-Allow-Methods', 'POST');
-res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     console.log(req.body)
-    console.log(req)
+    console.log(req.method)
+    if(req.method === "POST"){
         const data = req.body;
         if( data.login === process.env.NEXT_PUBLIC_ADMIN && data.password === process.env.NEXT_PUBLIC_ADMIN_PASSWORD){
             res.status(200).json([{status:1},{idRoom:undefined}])
@@ -26,4 +25,7 @@ res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
                 res.status(403).json([{status:0},{idRoom:undefined}])
             }
         }
+    }else{
+        res.status(405).json({message:"Це не для цього"})
+    }
 }
