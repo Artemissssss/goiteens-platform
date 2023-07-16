4e32a6b9c43fe11a2f40ac59d23ee5c5035452e6
Artem Vainshtein <64923345+Artemissssss@users.noreply.github.com>
2023-07-16 19:53:15 +0200

    Update authorization.js

--- src/pages/api/authorization.js
+++ src/pages/api/authorization.js
@@ -2,6 +2,8 @@ const { MongoClient, ObjectId } = require('mongodb');
 
 export default async function handler(req, res) {
  res.setHeader('Access-Control-Allow-Origin', '*');
+res.setHeader('Access-Control-Allow-Methods', 'POST');
+res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     
     if(req.method === "POST"){
         const data = req.body;
