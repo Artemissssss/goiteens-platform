f1e07f41c159e4e9b860b1f9cf74d2a23715e482
Artem Vainshtein <64923345+Artemissssss@users.noreply.github.com>
2023-07-17 14:17:11 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,4 +3,26 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/api/authorization',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: 'https://goiteens-platform-doc.vercel.app',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'Content-Type',
+          },
+        ],
+      },
+    ];
+  },
+};
