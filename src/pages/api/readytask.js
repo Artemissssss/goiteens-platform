0c5210c9d039c5d214f6fd47c1112357ba1ebf72
Artem Vainshtein <64923345+Artemissssss@users.noreply.github.com>
2023-07-17 14:54:39 +0200

    Update readytask.js

--- src/pages/api/readytask.js
+++ src/pages/api/readytask.js
@@ -1,7 +1,12 @@
 const { MongoClient } = require('mongodb');
 
 export default async function handler(req, res) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    if (req.method === 'OPTIONS') {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+        res.status(204).end();
+    } else {
     if(req.method === "POST"){
         const client = await MongoClient.connect(
             `mongodb+srv://${process.env.NEXT_PUBLIC_DATABASE_USER}:${process.env.NEXT_PUBLIC_DATABASE_PASSWORD}@${process.env.NEXT_PUBLIC_DATABASE}/?retryWrites=true&w=majority`,
@@ -26,5 +31,5 @@ export default async function handler(req, res) {
         res.status(200)
     }else{
         res.status(405).json({message:"Це не для цього"})
-    }
+    }}
 }
