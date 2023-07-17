70a67a8d1bcaa819862b19c7e1c969ef189f23c9
Artem Vainshtein <64923345+Artemissssss@users.noreply.github.com>
2023-07-17 13:46:49 +0200

    Update authorization.js

--- src/pages/api/authorization.js
+++ src/pages/api/authorization.js
@@ -1,37 +1,74 @@
+// const { MongoClient, ObjectId } = require('mongodb');
+
+
+// export default async function handler(req, res) {
+//     const allowedOrigins = ['https://goiteens-platform-doc.vercel.app', 'http://localhost:3000'];
+//   const origin = req.headers.origin;
+//   if (allowedOrigins.includes(origin)) {
+//        res.setHeader('Access-Control-Allow-Origin', origin);
+//   }
+//       // res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+//   // res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+//     console.log(req.body)
+//     console.log(req.method)
+//     if(req.method === "POST"){
+//         const data = req.body;
+//         if( data.login === process.env.NEXT_PUBLIC_ADMIN && data.password === process.env.NEXT_PUBLIC_ADMIN_PASSWORD){
+//             res.status(200).json([{status:1},{idRoom:undefined}])
+//         }else{
+//             const client = await MongoClient.connect(
+//                 `mongodb+srv://${process.env.NEXT_PUBLIC_DATABASE_USER}:${process.env.NEXT_PUBLIC_DATABASE_PASSWORD}@${process.env.NEXT_PUBLIC_DATABASE}/?retryWrites=true&w=majority`,
+//                 { useNewUrlParser: true, useUnifiedTopology: true }
+//                 );  
+//             const coll = client.db('goit-platform').collection('users');
+//             const filter = {"login": data.login};
+//             const cursor = coll.find(filter);
+//             const result = await cursor.toArray();
+//             await client.close();
+//             if(result[0] && result[0].password === data.password){
+//                 res.status(200).json([{status:result[0].status},{idRoom:result[0].idRoom}])
+//             }else{
+//                 res.status(403).json([{status:0},{idRoom:undefined}])
+//             }
+//         }
+//     }else{
+//         res.status(405).json({message:"Це не для цього"})
+//     }
+// }
 const { MongoClient, ObjectId } = require('mongodb');
 
-
 export default async function handler(req, res) {
-    const allowedOrigins = ['https://goiteens-platform-doc.vercel.app', 'http://localhost:3000'];
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-       res.setHeader('Access-Control-Allow-Origin', origin);
-  }
-      // res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
-  // res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    console.log(req.body)
-    console.log(req.method)
-    if(req.method === "POST"){
-        const data = req.body;
-        if( data.login === process.env.NEXT_PUBLIC_ADMIN && data.password === process.env.NEXT_PUBLIC_ADMIN_PASSWORD){
-            res.status(200).json([{status:1},{idRoom:undefined}])
-        }else{
-            const client = await MongoClient.connect(
-                `mongodb+srv://${process.env.NEXT_PUBLIC_DATABASE_USER}:${process.env.NEXT_PUBLIC_DATABASE_PASSWORD}@${process.env.NEXT_PUBLIC_DATABASE}/?retryWrites=true&w=majority`,
-                { useNewUrlParser: true, useUnifiedTopology: true }
-                );  
-            const coll = client.db('goit-platform').collection('users');
-            const filter = {"login": data.login};
-            const cursor = coll.find(filter);
-            const result = await cursor.toArray();
-            await client.close();
-            if(result[0] && result[0].password === data.password){
-                res.status(200).json([{status:result[0].status},{idRoom:result[0].idRoom}])
-            }else{
-                res.status(403).json([{status:0},{idRoom:undefined}])
-            }
-        }
-    }else{
-        res.status(405).json({message:"Це не для цього"})
-    }
+const allowedOrigins = ['*'];
+const origin = req.headers.origin;
+if (allowedOrigins.includes(origin)) {
+res.setHeader('Access-Control-Allow-Origin', origin);
+}
+// res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+// res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+console.log(req.body)
+console.log(req.method)
+if(req.method === "POST"){
+const data = req.body;
+if( data.login === process.env.NEXT_PUBLIC_ADMIN && data.password === process.env.NEXT_PUBLIC_ADMIN_PASSWORD){
+res.status(200).json([{status:1},{idRoom:undefined}])
+}else{
+const client = await MongoClient.connect(
+`mongodb+srv://${process.env.NEXT_PUBLIC_DATABASE_USER}:${process.env.NEXT_PUBLIC_DATABASE_PASSWORD}@${process.env.NEXT_PUBLIC_DATABASE}/?retryWrites=true&w=majority`,
+{ useNewUrlParser: true, useUnifiedTopology: true }
+);
+
+const coll = client.db('goit-platform').collection('users');
+const filter = {"login": data.login};
+const cursor = coll.find(filter);
+const result = await cursor.toArray();
+await client.close();
+if(result[0] && result[0].password === data.password){
+res.status(200).json([{status:result[0].status},{idRoom:result[0].idRoom}])
+}else{
+res.status(403).json([{status:0},{idRoom:undefined}])
+}
+}
+}else{
+res.status(405).json({message:"Це не для цього"})
+}
 }
